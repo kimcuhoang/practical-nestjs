57a0239f231bd299bf89d937cd132edba05e9245
Kim-CH <kim.cuhoang@gmail.com>
2025-07-24 10:17:07 +0700

    remove redundant code

--- src/w-hra-modules/sale-orders/use-cases/commands/assign-shipment/assign-shipment.handler.ts
+++ src/w-hra-modules/sale-orders/use-cases/commands/assign-shipment/assign-shipment.handler.ts
@@ -3,8 +3,7 @@ import { AssignShipmentCommand } from "./assign-shipment.command";
 import { InjectRepository } from "@nestjs/typeorm";
 import { SaleOrder } from "@src/w-hra-modules/sale-orders/domain";
 import { In, Repository } from "typeorm";
-import { Propagation, runInTransaction, Transactional } from "typeorm-transactional";
-import { plainToInstance } from "class-transformer";
+import { Propagation, runInTransaction } from "typeorm-transactional";
 
 @CommandHandler(AssignShipmentCommand)
 export class AssignShipmentHandler implements ICommandHandler<AssignShipmentCommand, void> {
@@ -14,29 +13,25 @@ export class AssignShipmentHandler implements ICommandHandler<AssignShipmentComm
         private readonly saleOrderRepository: Repository<SaleOrder>
     ) { }
 
-    @Transactional()
     public async execute(command: AssignShipmentCommand): Promise<void> {
         await runInTransaction(async () => {
 
-            const saleOrdersRaw = await this.saleOrderRepository.find({
+            const saleOrders = await this.saleOrderRepository.find({
                 where: { saleOrderCode: In(command.saleOrderCodes) },
                 relations: {
                     shipmentHistories: true
                 }
             });
 
-            if (!saleOrdersRaw?.length) {
+            if (!saleOrders?.length) {
                 throw new Error(`Sale orders with codes ${command.saleOrderCodes.join(", ")} not found`);
             }
 
-            const saleOrders = saleOrdersRaw
-                    .map(so => plainToInstance(SaleOrder, so));
-
-            for (const saleOrder of saleOrdersRaw) {
+            for (const saleOrder of saleOrders) {
                 saleOrder.assignToShipment(command.shipmentCode);
             }
 
-            await this.saleOrderRepository.save(saleOrdersRaw);
+            await this.saleOrderRepository.save(saleOrders);
             
         }, { propagation: Propagation.SUPPORTS }); // only one transaction
     }
